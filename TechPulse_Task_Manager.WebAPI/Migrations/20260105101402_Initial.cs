b199ac0f8be7c36da7565f623c26a471e08c23b6
omsalekar <omsalekar6052@gmil.com>
2026-01-05 16:20:00 +0530

    Initial setup: configuration and migrations

---
+++ TechPulse_Task_Manager.WebAPI/Migrations/20260105101402_Initial.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechPulse_Task_Manager.WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployeeTasks",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Designation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsWorkingOnProject = table.Column<bool>(type: "bit", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeTasks", x => x.EmployeeId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeTasks");
+        }
+    }
+}
---
+++ TechPulse_Task_Manager.WebAPI/Models/AuditLogs.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechPulse_Task_Manager.WebAPI.Models
+{
+    public class AuditLogs
+    {
+        [Key]
+        public int LogId { get; set; }
+        public int EmployeeId { get; set; }
+        [ForeignKey(nameof(EmployeeId))]
+        public Employees Employee { get; set; }
+        public string Action { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public string Remarks { get; set; } = string.Empty;
+    }
+}
